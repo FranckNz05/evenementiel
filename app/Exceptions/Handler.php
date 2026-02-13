6ea0a124534c8ea79120e6ba08bec605fb87eaf9
FranckNz05 <francknz05@gmail.com>
2026-02-13 11:24:35 +0100

    Fix PSR-4 and Handler errors

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -21,6 +21,25 @@ class Handler extends ExceptionHandler
     ];
 
     /**
+     * A list of exception types with their corresponding custom log levels.
+     *
+     * @var array<class-string<\Throwable>, \Psr\Log\LogLevel::*>
+     */
+    protected $levels = [
+        //
+    ];
+
+    /**
+     * The list of the inputs that are never flashed for validation exceptions.
+     *
+     * @var array<int, string>
+     */
+    protected $dontReport = [
+        AuthenticationException::class,
+        ModelNotFoundException::class,
+    ];
+
+    /**
      * Register the exception handling callbacks for the application.
      */
     public function register(): void
@@ -28,12 +47,5 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
-
-        // Ignorer le rapport des erreurs d'authentification et de modèle non trouvé
-        // car elles sont courantes (sessions expirées, liens morts) et polluent les logs.
-        $this->dontReport([
-            AuthenticationException::class,
-            ModelNotFoundException::class,
-        ]);
     }
 }
